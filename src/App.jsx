a699a94140ae85a6111f0a72107e539ed9045612
watagassa <k23095kk@aitech.ac.jp>
2023-09-03 16:53:52 +0900

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sidebar from "./components/Sidebar";
 import ImageUploader from "./ImageUploader";
-//import ImageDownloader from './ImageDownloader'
-
 function App() {
   return (
     <div className="App">
       {/* uplodaer */}
-      <ImageUploader/>
+      <ImageUploader />
+      <Sidebar />
     </div>
   );
 }
 export default App;
+
